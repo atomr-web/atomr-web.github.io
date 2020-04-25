d0a5291b5500a018c976053a152a1be2037d60ee
atomr <atomr@MacBook-Air-atomr.local>
2020-04-25 16:14:35 +0500

    init filtred add

---
+++ react-filtred-products/build/precache-manifest.32b5b19f0b0cf8c37b813b15b4295ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db88428a88d95fb658aa29d7ceafda0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b5450c3716a7ee279fd",
+    "url": "/static/css/main.36e60718.chunk.css"
+  },
+  {
+    "revision": "65f47cd9266617d37024",
+    "url": "/static/js/2.07a67af3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07a67af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19902d0fc130d806597",
+    "url": "/static/js/3.741a500e.chunk.js"
+  },
+  {
+    "revision": "0b5450c3716a7ee279fd",
+    "url": "/static/js/main.bab71b83.chunk.js"
+  },
+  {
+    "revision": "163e5f02a573cc666408",
+    "url": "/static/js/runtime-main.7bfdf81e.js"
+  }
+]);
