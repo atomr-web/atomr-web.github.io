8aeab2fdbe970dcef33b310633dbf34ac5b84616
atomr <wildkayote@yandex.ru>
2020-06-18 12:16:56 +0500

    add router basename

---
+++ react-bear-book/precache-manifest.199dcd2d95c62ab00fc2e7a0ebaf59ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a51011c813f0d3a1b674cad861c934",
+    "url": "/react-bear-book/index.html"
+  },
+  {
+    "revision": "3b7261133c0c94db1329",
+    "url": "/react-bear-book/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "de9a4caeb1cfeca900f3",
+    "url": "/react-bear-book/static/css/main.4dc06250.chunk.css"
+  },
+  {
+    "revision": "3b7261133c0c94db1329",
+    "url": "/react-bear-book/static/js/2.520f5a34.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-bear-book/static/js/2.520f5a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9a4caeb1cfeca900f3",
+    "url": "/react-bear-book/static/js/main.fbd5066b.chunk.js"
+  },
+  {
+    "revision": "f42617f8990b34bfcc8a",
+    "url": "/react-bear-book/static/js/runtime-main.880d7f6a.js"
+  }
+]);
